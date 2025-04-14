a6dbcba6d6a8ed5331329381f3140818740d81ec
karkeeg <karkibibek642@gmail.com>
2025-04-14 10:08:07 +0545

    Initial Commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Bibek Routing" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <div>Bibek karki Homepage</div>
+    </>
+  );
 }
